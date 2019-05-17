2c0157e75cad01efca791b7d20e37a1bc91d2316
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-05-17 21:17:56 +0100

    Added palindrome.js which makes use of the Stack data structure to check if a given string is a palindrome or not

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+// const {Stack} = require('stack');
+const {Stack} = require('./stack');
+
+const isPalindrome = (str) => {
+    const letterStack = new Stack();
+    for (let letter of str) {
+        letterStack.push(letter);
+    }
+    
+    let reversed = ''
+    while (!letterStack.isEmpty()) {
+        reversed += letterStack.pop();
+    }
+
+    let msg = str == reversed ? ' is ' : ' is not ';
+    return `'${str}'` + msg + ' a palindrome.';
+};
+
+console.log(isPalindrome('titi'));
+console.log(isPalindrome('racecar'));
